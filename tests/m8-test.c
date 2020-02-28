aaba4b40e8f1e6aad3735d037a32011800eb7bd3
N-Storm <nstorm0.0@gmail.com>
2020-02-28 13:43:19 +0300

    Tests: added UART simple sender to test

--- tests/m8-test.c
+++ tests/m8-test.c
@@ -2,10 +2,33 @@
 #include <stdint.h>
 #include <util/delay.h>
 
+void USART_Init()
+{
+  /* Set baud rate to 57600 */
+  UBRRL = 34;
+  UCSRA = 1 << U2X;
+  /* Enable receiver and transmitter */
+  UCSRB = (1<<RXEN)|(1<<TXEN);
+  /* Set frame format: 8N1 */
+  UCSRC = (1<<URSEL)|(1<<UCSZ1)|(1<<UCSZ0);
+}
+
+void USART_Transmit( unsigned char data )
+{
+  /* Wait for empty transmit buffer */
+  while ( !( UCSRA & (1<<UDRE)) );
+  /* Put data into buffer, sends the data */
+  UDR = data;
+}
+
+
 int main() {
   DDRB = 1 << PB5;
+  USART_Init();
+  USART_Transmit('A');
   while (1) {
     PORTB ^= 1 << PB5;
+    USART_Transmit((PORTB & (1 << PB5) ? 'X' : 'O'));
     _delay_ms(1000);
   }
 }
