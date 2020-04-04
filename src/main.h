ffd2180abcc1191ced0b5fb51569eb0cd4991817
nstor <nstor@NSTORM-WIN10>
2020-04-04 21:36:57 +0300

    Added stub MPLAB X Project. Added WS2812 library & settings.

---
+++ src/main.h
@@ -0,0 +1,23 @@
+/* 
+ * File:   main.h
+ * Author: nstor
+ *
+ * Created on 4 апреля 2020 г., 21:22
+ */
+
+#ifndef MAIN_H
+#define	MAIN_H
+
+#ifdef	__cplusplus
+extern "C" {
+#endif
+
+
+
+
+#ifdef	__cplusplus
+}
+#endif
+
+#endif	/* MAIN_H */
+
